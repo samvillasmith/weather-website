0c2d980f519270cff2bbee8ff3716f2b663742c0
samvillasmith <samuel.villa.smith@hotmail.com>
2022-01-03 19:26:08 -0600

    Removed console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side JavaScript file loaded')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
